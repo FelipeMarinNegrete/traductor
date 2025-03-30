245e51f2d4acd5240ca1793be81ed9e2391744af
FelipeMarinNegrete <163420412+FelipeMarinNegrete@users.noreply.github.com>
2025-03-30 14:02:22 -0300

    Update chat.js

--- Frontend/chat.js
+++ Frontend/chat.js
@@ -2,6 +2,7 @@ import AudioRecorder from "./audioRecorder.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const recorder = new AudioRecorder(); // Instanciamos el grabador
+    console.log(recorder);
 
     document.getElementById("startRecording").addEventListener("click", () => {
         recorder.startRecording();
